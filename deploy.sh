4bac0a35f7dd1cd3a2bda411830be9b02138226b
harold1024 <744924498@qq.com>
2019-12-20 17:02:41 +0800

    Signed-off-by: harold1024 <744924498@qq.com>

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run build
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
-git push -f git@github.com:harold1024/vuepress.git master:gh-pages
+git push -f git@github.com:harold1024/blog.git master:gh-pages
 
 cd -
