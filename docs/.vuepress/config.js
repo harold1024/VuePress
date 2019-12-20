7fdd39ebf386c21d545a837958ecbc74915497f9
harold1024 <744924498@qq.com>
2019-12-20 16:31:59 +0800

    perfect function&config ===> first Article Signed-off-by: harold1024 <744924498@qq.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  title: '博客标题',
+  title: 'Harold\'s blog',
   description: '描述',
   head: [ // 注入到当前页面的 <head> 中的标签
-    ['link', { rel: 'icon', href: '/images/logo.png' }],
-    ['link', { rel: 'manifest', href: '/images/logo.png' }],
-    ['link', { rel: 'apple-touch-icon', href: '/images/logo.png' }],
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/logo.png' }],
+    ['link', { rel: 'apple-touch-icon', href: '/logo.png' }],
   ],
   serviceWorker: true, // 是否开启 PWA
   base: '/blog/', // 部署到github相关的配置
@@ -14,16 +14,13 @@ module.exports = {
   themeConfig: {
     // 导航栏配置
     nav:[
-      {text: 'xxx', link: '/demo/' },
+      {text: 'Home', link: '/' },
+      {text: 'Learn', link: '/learn/' },
+      {text: 'Profile', link: '/profile/' },
+      {text: 'vue', link: '/vue/' },
+      {text: 'react', link: '/react/' },
+      {text: '风花雪月', link: '/风花雪月/' }
     ],
-    sidebar:[
-      {
-        title: '侧边栏名称', // 侧边栏名称
-        collapsable: true, // 可折叠
-        children: [
-          '/md文件地址/', // 你的md文件地址
-        ]
-      },
-    ]
+    version: '0.11.0-beta'
   }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,16 @@
-# Hello Vuepress
+---
+home: true
+heroImage: /logo.png
+heroText: JUST DO IT
+tagline: fighting for the life I want.
+actionText: Get Started →
+actionLink: /learn/
+features:
+- title: Blogger First
+  details: This framework optimizes the VuePress engine for blogging first and foremost. Includes default features like RSS feed generation, list of recent posts, etc.
+- title: Simplicity First
+  details: Minimal setup with markdown-centered project structure helps you focus on writing.
+- title: Vue.js Powered Engine
+  details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop your theme with Vue
+footer: MIT Licensed | Created by BenCodeZen
+---
